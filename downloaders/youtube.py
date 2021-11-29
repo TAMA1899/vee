5d1499cca1f66338116fcdf82ec62bfa919bbac4
TAMA1899 <60563853+TAMA1899@users.noreply.github.com>
2021-11-29 15:16:31 +0700

    Update youtube.py

--- downloaders/youtube.py
+++ downloaders/youtube.py
@@ -5,6 +5,7 @@ from yt-dlp.yt-dlp import YoutubeDL
 from config import DURATION_LIMIT
 from helpers.errors import DurationLimitError
 
+
 ydl_opts = {
     "format": "bestaudio/best",
     "geo-bypass": True,
